cd8d4684d3bfd78324127aad8a07a6484d246f95
JayDeffault <kiril-aka@mail.ru>
2026-06-14 03:14:18 +0600

    Restore PHAT body movement with dents

--- Source/Deformation/Private/Deformation.cpp
+++ Source/Deformation/Private/Deformation.cpp
@@ -6,15 +6,12 @@
 
 void FDeformationModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
 void FDeformationModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FVehicleModule, Deformation)
+
+IMPLEMENT_MODULE(FDeformationModule, Deformation)
